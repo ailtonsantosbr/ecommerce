f09f13a41653845188d082c4d1e0a244484d975d
ailtonsantosbr <ailton.santosbr@gmail.com>
2020-01-22 21:50:24 -0300

    Criamos o Admin views

--- index.php
+++ index.php
@@ -4,21 +4,23 @@ require_once "vendor/autoload.php";
 
 use \Hcode\Page;
 use \Slim\Slim;
+use \Hcode\PageAdmin;
 
-$app = new \Slim\Slim();
+$app = new Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function () {
-
-	// echo "OK";
-	// $sql = new Hcode\DB\Sql();
-
-	// $results = $sql->select("SELECT * FROM tb_users");
-
-	// echo json_encode($results);
+$app->get('/', function() {
 
 	$page = new Page();
+
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
